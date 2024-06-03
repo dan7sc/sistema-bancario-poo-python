ae9178778c7f53a76b3ae3a7af68da122cdc444c
dan7sc <dan7sc@users.noreply.github.com>
2024-06-03 15:45:59 -0300

    implementa sistema bancario

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ menu = """
 => """
 
 saldo = 0
-limite = 500
+LIMITE = 500
 extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
@@ -18,13 +18,44 @@ while True:
     opcao = input(menu)
 
     if opcao == "d":
-        print("Depósito")
+        deposito = -1 
+        try:
+            deposito = float(input("Digite o valor do depósito: "))
+        except ValueError:
+            pass
+
+        if deposito > 0:
+            saldo += deposito
+            extrato += f"Depósito: R$ {deposito:.2f}\n"
+        else:
+            print("Operação inválida: valor inválido.")
 
     elif opcao == "s":
-        print("Saque")
+        saque = -1
+        try:
+            saque = float(input("Digite o valor do saque: "))
+        except ValueError:
+            pass
+
+        if numero_saques >= LIMITE_SAQUES:
+            print("Operação inválida: Limite diário de saques atingido.")
+        elif saque > LIMITE:
+            print("Operação inválida: Limite máximo de R$ 500.00 por saque.")
+        elif saque > saldo:
+            print("Operação inválida: Saldo insuficiente.")
+        elif saque > 0:
+            saldo -= saque
+            numero_saques += 1
+            extrato += f"Saque: R$ {saque:.2f}\n"
+        else:
+            print("Operação inválida: valor inválido.")
 
     elif opcao == "e":
-        print("Extrato")
+        if extrato == "":
+            print("Não foram realizadas movimentações.")
+        else:
+            mensagem = f"{extrato}Saldo: R$ {saldo:.2f}"
+            print(mensagem)
 
     elif opcao == "q":
         break
